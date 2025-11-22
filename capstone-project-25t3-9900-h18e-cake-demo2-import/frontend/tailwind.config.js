962e2064b961a547fea8819bedcb218cd7bbc5ac
robertdyx <robertdyx@gmail.com>
2025-11-22 13:19:33 +1100

    Delete capstone-project-25t3-9900-h18e-cake-demo2-import directory

--- capstone-project-25t3-9900-h18e-cake-demo2-import/frontend/tailwind.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{ts,tsx}"],
-  theme: { extend: { colors: { blueberry: "#00007a" } } },
-  plugins: [],
-}
